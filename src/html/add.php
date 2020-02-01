f04cc009c7024c5a835c76d1b70ffd2deb0c5939
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2020-02-01 10:40:21 +0900

    コメントを充実させた。

--- src/html/add.php
+++ src/html/add.php
@@ -1,36 +1,43 @@
 <?php
-    // サニタイジング
-    $post = array();
-    foreach ($_POST as $k => $v) {
-        $post[$k] = htmlspecialchars($v, ENT_QUOTES, 'UTF-8');
-        // $post[$k] = $v;
-    }
+// サニタイジングを行う
+// HTMLやJavascriptとして意味のある文字を別の文字（HTMLエンティティ）に変換する。
+// https://www.php.net/manual/ja/function.htmlspecialchars.php
+$post = array();
+foreach ($_POST as $k => $v) {
+    $post[$k] = htmlspecialchars($v, ENT_QUOTES, 'UTF-8');
+    // $post[$k] = $v;
+}
 
-    try {
-        // データベースに接続
-        $dsn = 'mysql:dbname=todo_list;host=localhost;charset=utf8';
-        $dbh = new PDO($dsn, 'root', 'root');
-        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try {
+    // データベースに接続
+    $dsn = 'mysql:dbname=todo_list;host=localhost;charset=utf8';
+    $dbh = new PDO($dsn, 'root', 'root');
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // POSTデータをデータベースにインサートする
-        $sql = '';
-        $sql .= 'insert into todo_items (';
-        $sql .= 'expiration_date,';
-        $sql .= 'todo_item';
-        $sql .= ') values (';
-        $sql .= ':expiration_date,';
-        $sql .= ':todo_item';
-        $sql .= ')';
+    // POSTデータをデータベースにインサートする
+    $sql = '';
+    $sql .= 'insert into todo_items (';
+    $sql .= 'expiration_date,';
+    $sql .= 'todo_item';
+    $sql .= ') values (';
+    $sql .= ':expiration_date,';
+    $sql .= ':todo_item';
+    $sql .= ')';
 
-        $stmt = $dbh->prepare($sql);
-        $stmt->bindValue(':expiration_date', $post['expiration_date'], PDO::PARAM_STR);
-        $stmt->bindValue(':todo_item', $post['todo_item'], PDO::PARAM_STR);
-        $stmt->execute();
+    $stmt = $dbh->prepare($sql);
 
-        // 処理が完了したらトップページへリダイレクト
-        header('Location: ./');
+    // bindValue()の使い方は、下記を参照のこと
+    // https://www.php.net/manual/ja/pdostatement.bindvalue
+    // 「例1 名前付けされたプレースホルダを用いてプリペアドステートメントを実行する」のところ
+    $stmt->bindValue(':expiration_date', $post['expiration_date'], PDO::PARAM_STR);
+    $stmt->bindValue(':todo_item', $post['todo_item'], PDO::PARAM_STR);
+    $stmt->execute();
 
-    } catch (Exception $e) {
-        var_dump($e);
-        exit;
-    }
+    // 処理が完了したらトップページ（index.php）へリダイレクト
+    header('Location: ./');
+} catch (Exception $e) {
+    // 例外発生時の処理
+
+    var_dump($e);
+    exit;
+}
--- src/html/index.php
+++ src/html/index.php
@@ -1,11 +1,18 @@
 <?php
+// コーディングのヒント
+// できるだけPHPのコードとHTMLのコードは分けて書く。
+// 自分でも、他の人が見ても、わかりやすいように、適切にコメントを書く。
+
 try {
     // データベースに接続
     $dsn = 'mysql:dbname=todo_list;host=localhost;charset=utf8';
+    // データベースに接続するためのユーザー名・パスワードは、自分の開発環境のMySQLの設定を確認して編集する
+    // XAMPPの場合は、デフォルトでパスワードなし
+    // MAMPの場合は、デフォルトでパスワードは「root」
     $dbh = new PDO($dsn, 'root', 'root');
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // 登録済みのTODOリストを取得
+    // 削除済みを除く登録済みのTODOリストを全件取得
     $sql = '';
     $sql .= 'select ';
     $sql .= 'id, ';
@@ -20,8 +27,11 @@ try {
 
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
+
+    // 取得したレコードを連想配列として変数に代入する
     $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 } catch (Exception $e) {
+    // 例外発生時の処理
     var_dump($e);
     exit;
 }
@@ -32,7 +42,9 @@ try {
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>TODOリスト</title>
+    <!-- ブラウザの種類によって見え方の差異をなくすためのCSS -->
     <link rel="stylesheet" href="./css/normalize.css">
+    <!-- 　TODOリストの見栄えを変えるためのCSS -->
     <link rel="stylesheet" href="./css/main.css">
 </head>
 
@@ -54,17 +66,22 @@ try {
                     <th>削除</th>
                     <th></th>
                 </tr>
+                <!-- 取得したレコード分繰り返し処理を行う -->
                 <?php foreach ($list as $v) : ?>
                     <tr>
                         <form action="action.php" method="POST">
+                            <!-- 画面に表示しないformの項目は、「type="hidden"」にする。 -->
                             <input type="hidden" name="id" value="<?= $v['id'] ?>">
                             <?php if ($v['is_completed'] == 1) : ?>
+                                <!-- is_completed＝1のときは、打ち消し線が入るようにCSSのクラスを付与する -->
                                 <td class="del"><?= $v['expiration_date'] ?></td>
                                 <td class="del"><?= $v['todo_item'] ?></td>
                             <?php else : ?>
+                                <!-- is_completed=0のときは、そのまま表示する -->
                                 <td><?= $v['expiration_date'] ?></td>
                                 <td><?= $v['todo_item'] ?></td>
                             <?php endif ?>
+                            <!-- ラジオボタンは、is_completed=0のときは「未完了」が選択状態に、is_completed=1のときは「完了」が選択状態になるようにする -->
                             <td class="center"><input type="radio" name="is_completed" value="0" <?php if ($v['is_completed'] == 0) echo ' checked' ?>></td>
                             <td class="center"><input type="radio" name="is_completed" value="1" <?php if ($v['is_completed'] == 1) echo ' checked' ?>></td>
                             <td class="center"><input type="checkbox" name="is_deleted"></td>
